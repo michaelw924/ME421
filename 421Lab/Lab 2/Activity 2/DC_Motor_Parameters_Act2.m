8ca332b3536fffbe46a0b13843b34b0a179cea5e
michaelw924 <michaelwhite924@gmail.com>
2021-03-23 19:34:18 -0500

    Finished w/ activity 2 and making progress on 3

--- 421Lab/Lab 2/Activity 2/DC_Motor_Parameters_Act2.m
+++ 421Lab/Lab 2/Activity 2/DC_Motor_Parameters_Act2.m
@@ -1,4 +1,4 @@
-%% Activity 3
+%% Activity 2
 % 3/5/21
 % Michael White
 % Section 3 / Online
@@ -9,13 +9,13 @@ clc;
 % Define parameters to be used
 Kt = 7.68e-3;
 Kb = 7.68e-3;
-L = 0;
+L = 0.18e-3;
 J = 3.9e-7;
 bm = 8.148e-7;
 Ra = 2.6;
 
 % Run the simulation
-simout = sim('DC_Motor_Sim_Act3');
+simout = sim('Activity_2_sim');
 
 % Generate figure and plot results
 figure;
@@ -23,6 +23,20 @@ hold on;
 plot(simout.Speed);
 xlabel('Time (sec)');
 ylabel('Angular Velocity (rad/sec)');
+
+% WRITTEN RESPONSES:
+
+% This system is clearly a first order system response, as it is simply
+% rising up to the maximum value in each trial, and not overshooting. 
+% In addition, the governing function is a first order equation.
+
+% This activity asks for the max RPM that the motor reaches, but the output
+% for this system is in radians per second. This conversion and output can
+% be seen below, with the result being written to the command window.
+
+% Calculating the max RPM (including conversion from rad/sec)
+maxRPM = max(simout.Speed.Data)*9.5492965;
+disp(strcat("The max RPM reached by the motor is ",num2str(maxRPM)));
 
 % Isolating the data to the first curve (contained in first 0.2 seconds)
 calculationData = simout.Speed.Data(simout.Speed.Time <= 0.2);
@@ -35,8 +49,6 @@ absDiffList = abs(calculationData-timeConstantValue);
 timeConstantPoint = ...
     [calculationTime(absDiffList == min(absDiffList)),...
      calculationData(absDiffList == min(absDiffList))];
-
-% Compare this time constant to Activity 2
-timeConstant_Act2 = 0.0165;
-percentDiff = 100*(abs(timeConstantPoint(1)-timeConstant_Act2)/mean([timeConstant_Act2,timeConstantPoint(1)]));
-disp(strcat("The percent difference in time constants between Activity 2 and Activity 3 is ",num2str(percentDiff),"%"));
+ 
+% Display the calculated time constant
+disp(strcat("The time constant of the system is ",num2str(timeConstantPoint(1))));
